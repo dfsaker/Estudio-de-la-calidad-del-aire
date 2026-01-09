e92416460b452087e0586d4225999a1c01f47f2f
dfsaker <dfsaker7@gmail.com>
2026-01-09 18:56:12 -0300

    dato

--- Scripts/estudio.py
+++ Scripts/estudio.py
@@ -2,4 +2,31 @@ import pandas as pd
 
 dataframe = pd.read_csv("city_air_quality.csv")
 
+dataframe_2 = pd.read_csv("tablaEPA.csv")
+
+print(dataframe.info())
+
+print('--------------------------------------------')
+
 print(dataframe.head())
+
+#Eliminar filas desde 367 en adelante
+
+dataframe.drop(dataframe.index[365:], inplace=True)
+
+print(dataframe.info())
+
+
+
+# Asegurarse de que las fechas estén en formato datetime.
+
+dataframe['Date'] = pd.to_datetime(dataframe['Date'])
+
+# Eliminar outliers en concentraciones de gases.
+
+# Crear un índice de calidad del aire (AQI simplificado).
+
+print(dataframe_2)
+
+dataframe.to_csv("city_air_quality_.csv")
+
