35b5991587347df663099adcb6977dbbb55d6501
dfsaker <dfsaker7@gmail.com>
2026-02-24 19:56:30 -0300

    nuevo

---
+++ Scripts/estudio2.py
@@ -0,0 +1,10 @@
+#Análisis exploratorio con Python:
+
+
+#Calcular promedios mensuales de cada contaminante.
+
+#Analizar correlación entre temperatura, humedad y contaminación.
+
+#Determinar qué ciudades superan los niveles recomendados por la OMS.
+
+
